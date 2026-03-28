d0039558be144a10673b1b6dd7a7cedc40b5a7f4
Darren Foo <stonith@users.noreply.github.com>
2026-03-27 20:49:45 -0700

    Fix GasBuddy mobile-orchestration URL encoding

--- pyfuelprices/sources/usa/gasbuddy.py
+++ pyfuelprices/sources/usa/gasbuddy.py
@@ -4,6 +4,7 @@ import json
 import uuid
 
 from geopy import distance
+from yarl import URL
 
 from pyfuelprices.fuel_locations import Fuel, FuelLocation
 
@@ -46,8 +47,9 @@ class GasBuddyUSASource(Source):
 
     async def _send_request(self, url) -> str:
         """Send a request to the API and return the raw response."""
+        request_url = URL(url, encoded=True) if "mobile-orchestration" in url else url
         _LOGGER.debug("Sending HTTP request to GasBuddy with URL %s", url)
-        async with self._client_session.get(url=url,
+        async with self._client_session.get(url=request_url,
                                             headers=self._headers) as response:
             if response.ok:
                 return await response.text()
